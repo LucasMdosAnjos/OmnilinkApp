07a942852bfb5e10ce6b09fe2bebeeecc3af56e1
LucasMdosAnjos <lucasmon@live.com>
2020-05-15 19:21:58 -0300

    Ajuste

--- lib/app/modules/bluetooth/bluetooth_controller.dart
+++ lib/app/modules/bluetooth/bluetooth_controller.dart
@@ -485,6 +485,7 @@ abstract class _BluetoothControllerBase with Store {
                     .toList()[0]
                     .split(',')[1];
                 int indice = int.tryParse(ultimaRequisicao.split(',')[1]);
+                widgets.clear();
                 Modular.to.pushNamed('/formMsgFormatada', arguments: {
                   'titulo': titulo,
                   'formFields': formulario,
